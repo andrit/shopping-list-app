849d85875cb53cef548bb55c538490087cba36b7
andrit <arnewmediatech@gmail.com>
2014-10-29 19:49:11 -0400

    jquery and favicon

--- js/appscript.js
+++ js/appscript.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-	console.log("add button clicked")
+	var add = ($(this).parent('.input-input').find('.add-item').val();)
 	$('.add-button').on('click', function(e){
         e.preventDefault();
         console.log("add button clicked")
-        $(this).parent('.input-input').find('.add-item').val();
-        
-//put new items into DOM and on list
+ 
         $('.list-list').append('<button class="checked"><i class="fa fa-check fa-2x"></i></button>
                 <p class="list-item-name">' + add + '</p>
                 <button class="remove">X</button>');
