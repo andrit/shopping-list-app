fac224b14362136e06afedaa103579fff469360d
andrit <arnewmediatech@gmail.com>
2014-10-29 20:56:19 -0400

    add button jquery

--- js/appscript.js
+++ js/appscript.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-	var add = ($(this).parent('.input-input').find('.add-item').val();)
+	var add = $(this).parent('.input-input').find('.add-item').val();
 	$('.add-button').on('click', function(e){
         e.preventDefault();
-        console.log("add button clicked")
- 
-        $('.list-list').append('<button class="checked"><i class="fa fa-check fa-2x"></i></button>
-                <p class="list-item-name">' + add + '</p>
-                <button class="remove">X</button>');
+        console.log("add button clicked");
+ 			
+        $('.list-list').append('<button class="checked"><i class="fa fa-check fa-2x"></i></button>' +
+                '<p class="list-item-name">' + add + '</p>' +
+                '<button class="remove">X</button>');
+});
 });
