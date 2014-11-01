c940e0b9c00b37d11cf2bfd0aa064cc34c8be330
andrit <arnewmediatech@gmail.com>
2014-11-01 17:29:24 -0400

    jquery Add

--- js/appscript.js
+++ js/appscript.js
@@ -5,7 +5,21 @@ $(document).ready(function() {
         console.log("add button clicked");
  			
         $('.list-list').append('<button class="checked"><i class="fa fa-check fa-2x"></i></button>' +
-                '<p class="list-item-name">' + add + '</p>' +
-                '<button class="remove">X</button>');
+                '<li><p class="list-item-name">' + add + '</p>' +
+                '<button class="remove">X</button><p class="extend-list-item">More Info <i class="fa fa-chevron-down"></i></p></li>');
 });
+
+	$('#reset-button').on('click', function(){
+		$('.list-list').find('.list-item').remove();
+});
+
+	$('.checked').on('click', function(){
+		$(this).closest('.list-item').find('list-item-name').addClass("line-through");
+});
+	$('.remove').on('click', function(){
+		$(this).closest(.list-item).remove();
+	})
+
+
+
 });
