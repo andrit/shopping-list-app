de66c0f265f53f1f59b466d37d83531986485bc8
andrit <arnewmediatech@gmail.com>
2014-11-05 21:54:00 -0500

    make changes to js and css on gh-pages and main

--- js/appscript.js
+++ js/appscript.js
@@ -2,27 +2,27 @@ $(document).ready(function() {
 	var add = $(this).parent('.input-input').find('.add-item').val();
 	$('.add-button').on('click', function(e){
         e.preventDefault();
-        console.log("add button clicked")
- 
+        console.log("add button clicked");
+ 			
         $('.list-list').append('<button class="checked"><i class="fa fa-check fa-2x"></i></button>' +
-                '<p class="list-item-name">' + add + '</p>' +
-                '<button class="remove">X</button>');
-	});
-		$('#reset-button').on('click', function(){
-			console.log("reset button clicked");
-			$('.list-list').child('li').remove();
-	});
-		$('.prod-add-more-info').on('click', function(){
-			$(this).find('.input-slideout').show(slow);
-	});
-		$('.checked').on('click', function(){
-			console.log("Check complete button clicked");
-			$(this).closest('.list-item-name').addClass("line-through");
-	});
-		$('.remove').on('click', 'button', function(){
-			$(this).closest('.list-item').remove();
-	});
-		$('.extend-list-item').on('click', function(){
-			$(this).find('.more-info-slide-out').show(slow);
-	});
+                '<li class="list-item"><p class="list-item-name">' + add + '</p>' +
+                '<button class="remove">X</button><p class="extend-list-item">More Info <i class="fa fa-chevron-down"></i></p></li>');
+});
+
+	$('#reset-button').on('click', function(){
+		console.log("reset button clicked");
+		$('.list-list').find('.list-item').remove();
+});
+
+	$('.checked').on('click', function(){
+		console.log("checked button clicked");
+		$(this).closest('.list-item').find('list-item-name').addClass("line-through");
+});
+	$('.remove').on('click', function(){
+		console.log("remove button clicked");
+		$(this).closest(.list-item).remove();
+	})
+
+
+
 });
